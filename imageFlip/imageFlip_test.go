422e9b1f30952b0d5d2c685e59304899f6a92e09
Matthew Robertson <matt@matthewrobertson.co.uk>
2023-01-14 21:31:34 +0000

    tidy tests

--- imageFlip/imageFlip_test.go
+++ imageFlip/imageFlip_test.go
@@ -10,6 +10,7 @@ import (
 
 var b = color.Black
 var w = color.White
+	// expected output
 var output = [][]color.Color{
 	{b, b, b, w, w, w, w, w, b, b, b, b, b},
 	{b, b, w, w, b, b, b, w, w, b, b, b, b},
@@ -23,7 +24,7 @@ var output = [][]color.Color{
 	{b, b, w, b, w, w, w, b, w, b, b, b, b},
 	{b, b, b, b, b, b, w, b, b, b, b, b, b},
 }
-
+	// expected output
 var flipOuput = [][]color.Color{
 	{b, b, b, b, b, b, w, b, b, b, b, b, b},
 	{b, b, w, b, w, w, w, b, w, b, b, b, b},
@@ -45,12 +46,10 @@ func TestImageToGrid(t *testing.T) {
 	avatarGif, err := gif.DecodeAll(f)
 	f.Close()
 
-	// expected output
-
 	img := imageToGrid(avatarGif.Image[0])
 
     if !reflect.DeepEqual(img, output)  || err != nil {
-        t.Errorf(`imageToGrid(avatarGif.Image[0]) = %q, %q`, img,  output)
+        t.Errorf(`imageToGrid(avatarGif.Image[0]) = %q, and not %q`, img,  output)
     }
 }
 
@@ -62,13 +61,11 @@ func TestImageFlip(t *testing.T) {
 	avatarGif, err := gif.DecodeAll(f)
 	f.Close()
 
-	// expected output
-
 	resultImg := flip(avatarGif.Image[0])
 
-	result := imageToGrid(resultImg)
+	result := imageToGrid(resultImg) // the flip returns an image but it's easier to compare using grids.
 
     if !reflect.DeepEqual(result, flipOuput)  || err != nil {
-        t.Errorf(`imageToGrid(avatarGif.Image[0]) = %q, %q`, result,  flipOuput)
+        t.Errorf(`flip(avatarGif.Image[0]) = %q, and not %q`, result,  flipOuput)
     }
 }
