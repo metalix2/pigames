b149413f9c84fa402f4eecb79b14c5eb5c99db4a
Matthew Robertson <matt@matthewrobertson.co.uk>
2023-01-29 21:13:26 +0000

    how does this look

--- scenarios/render.go
+++ scenarios/render.go
@@ -2,6 +2,7 @@ package scenarios
 
 import (
     "fmt"
+    "log"
     "image"
     "image/color"
     "image/draw"
@@ -50,7 +51,7 @@ func createMaze(w, h int) *maze.Maze {
 	return maze
 }
 
-func getStringLen(stings [][]string) int {
+func getStringLen(stings []string) int {
     strLen := 0
     for di:=0; di < len(dialog); di++ {
         strLen += len(dialog[di])
@@ -208,32 +209,30 @@ func DrawEnding(w, h int, src, src2, src3 image.Image, prev_coords map[string]in
         next_coords["x"] = prev_coords["x"]
         next_coords["y"] = prev_coords["y"]
         strLen := getStringLen(dialog[0])
-    
-            for  str:=0; str < len(dialog[0]); str++ {
+        log.Println(strLen)
 
-                if subStrLen < strLen  {
-                    d.DrawString(dialog[0][str])
-                }
-                y := fixed.I(10+(0*35)+(str*11))
-                if len(dialog[0]) == 1 {
-                    y = fixed.I(32)
-                }
-                d := &font.Drawer{
-                    Dst:  img,
-                    Src:  image.NewUniform(color.White),
-                    Face: basicfont.Face7x13,
-                    Dot:  fixed.Point26_6{fixed.I(1), y},
-                }
-                subStrLen := getStringLen(dialog[0][:str])
-                log.Println(subStrLen)
-                
-                if introFrames < len(dialog[0][str]) {
-                    d.DrawString(dialog[0][str][:introFrames])
-                } else  {
-                    d.DrawString(dialog[0][str])
-                } 
+        for  str:=0; str < len(dialog[0]); str++ {
+
+            y := fixed.I(10+(0*35)+(str*11))
+            if len(dialog[0]) == 1 {
+                y = fixed.I(32)
             }
+            d := &font.Drawer{
+                Dst:  img,
+                Src:  image.NewUniform(color.White),
+                Face: basicfont.Face7x13,
+                Dot:  fixed.Point26_6{fixed.I(1), y},
+            }
+            subStrLen := getStringLen(dialog[0][:str])
+            log.Println(subStrLen)
+            
+            if introFrames < len(dialog[0][str]) {
+                d.DrawString(dialog[0][str][:introFrames])
+            } else  {
+                d.DrawString(dialog[0][str])
+            } 
         }
+    
     }
 
     // Draw Avatar and it's Orientation
