a501a229094c4ddb424d924c5beb97987e58284f
Matthew Robertson <matt@matthewrobertson.co.uk>
2023-01-29 22:11:56 +0000

    how does this look

--- scenarios/render.go
+++ scenarios/render.go
@@ -225,9 +225,8 @@ func DrawEnding(w, h int, src, src2, src3 image.Image, prev_coords map[string]in
             log.Println(introFrames - getStringLen(dialog[0][:str+1]) + 1)
              if introFrames > getStringLen(dialog[0][:str+1])  { // if intro > str len print that str
                 d.DrawString(dialog[0][str])
-             } else if introFrames < getStringLen(dialog[0][:str+1]) {
-                d.DrawString(dialog[0][str][:introFrames - getStringLen(dialog[0][:str+1]) + 1])
-             } else {
+             } else  {
+                d.DrawString(dialog[0][str][:introFrames - (getStringLen(dialog[0][:str+1]) - len(dialog[0][str])) + 1])
              }
            
         }
